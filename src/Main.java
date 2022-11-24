1015b48e30eace40f03c3d3aba41bdc0b12d6d1d
EdemKarashaev <115266061+EdemKarashaev@users.noreply.github.com>
2022-11-24 20:13:19 +0300

    madeLambda

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 
 public class Main {
@@ -11,12 +12,26 @@ public class Main {
         Person p4 = new Person("Pavel", "fon der Braun", 43);
         Person p5=new Person("Maria", "Galkina Voronova", 6);
 
+        Comparator<Person> CompPerson = (Person o2, Person o1) -> {
+            int j = o1.surname.split("(\\s+|-)").length;
+            int k = o2.surname.split("(\\s+|-)").length;
+            if (j > k) {
+                return -1;
+            } else if (j < k) {
+                return 1;
+            } else {
+                return o2.age - o1.age;
+            }
+        };
+
         sortPerson.add(p1);
         sortPerson.add(p2);
         sortPerson.add(p3);
         sortPerson.add(p4);
         sortPerson.add(p5);
-        Collections.sort(sortPerson, new CompPerson());
+        Collections.sort(sortPerson, CompPerson);
         System.out.println(sortPerson);
     }
 }
+
+
